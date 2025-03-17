4f502e95b438591ea8eb9d1467fe936051b31748
Paul Gajewski <gajewski@solandeo.com>
2025-03-17 23:10:13 +0100

    first own config
    colorscheme and python support

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -221,6 +221,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        pylsp = {},
 
         lua_ls = {
           -- cmd = { ... },
